4f34f1ecb23862ee7d3eea2954a0f503cdf7e2a8
realsarius <berkansozer@protonmail.com>
2023-08-04 10:32:38 +0300

    fixed query problem with hex value

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-console.log("Hello");
+import { getColors } from "./utils.js";
 const form = document.getElementById("colorSchemeForm");
 
 form.addEventListener("submit", (e) => {
+  const color = document.getElementById("colorInput").value.substring(1);
+  const option = document.getElementById("colorSelect").value;
+
+  getColors(color, option);
+
+  console.log(color);
+  console.log(option);
   e.preventDefault();
 });
